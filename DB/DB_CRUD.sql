17aed2593ce090135d93630dfe025414433b8d81
YefersonAgudel0 <yeferson.agudelo@codeskills.com.co>
2025-02-19 07:13:24 -0500

    Fix

--- DB/DB_CRUD.sql
+++ DB/DB_CRUD.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE DB_CRUD
 USE DB_CRUD
 
-CREATE TABLE inventario_fruta  (
-    id_fruta INT IDENTITY(1,1) PRIMARY KEY,
+CREATE TABLE fruta  (
+    id INT IDENTITY(1,1) PRIMARY KEY,
     nombre_fruta NVARCHAR(50) NOT NULL,
     cantidad INT NOT NULL CHECK (cantidad >= 0), -- Asegura que la cantidad no sea negativa
     precio_unitario INT NOT NULL CHECK (precio_unitario >= 0),
@@ -11,7 +11,7 @@ CREATE TABLE inventario_fruta  (
 );
 
 -- Insertar datos de ejemplo
-INSERT INTO inventario_fruta (nombre_fruta, cantidad, precio_unitario, proveedor, fecha_ingreso)
+INSERT INTO fruta (nombre_fruta, cantidad, precio_unitario, proveedor, fecha_ingreso)
 VALUES 
     ('Manzana', 50, 2500, 'Frutas del Valle', '2025-02-07'),
     ('Banano', 100, 1500, 'AgroFrutas', '2025-02-07'),
@@ -19,4 +19,4 @@ VALUES
     ('Naranja', 60, 2000, 'Frutas del Valle', '2025-02-07');
 
 -- Consultar los datos
-SELECT * FROM inventario_fruta;
+SELECT * FROM fruta;
